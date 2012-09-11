1c2eabec2837e8be4d439d1cf198aa54fc9d90cd
Andrew Brown <ajbpn7@mst.edu>
2012-09-11 00:39:29 -0500

    Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmnirailsStarter::Application.config.secret_token = 'e2fb4657c95d321e3cf01f8dc1aa49240dc20ab117a0ec9e3d1bc596a483266f0c52c59d995caab318c0df03e764fcc92b42b16c288f90f9f4ff61ae7fc37bda'
